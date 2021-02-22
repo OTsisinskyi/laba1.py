5f81e12d8205a2737436810e2d8708eef1023d00
OTsisinskyi <alextsisinsky@gmail.com>
2021-02-22 14:33:12 +0200

    corrected __str__

--- laba1.py
+++ laba1.py
@@ -1,7 +1,7 @@
 class Book:
     __count = 0
 
-    def __init__(self, author='', name='', rating=0, pages=0, price=0):
+    def __init__(self, author='', name='', rating=0.0, pages=0, price=0):
         Book.__count += 1
         self.rating = rating
         self.author = author
@@ -20,20 +20,17 @@ class Book:
     def get_count():
         return Book.__count
 
-    def __repr__(self):
-        return f"The object Book - {self.author}"
-
     def __del__(self):
         print('деструктор')
 
 
 book_1 = Book("Brian Tracy", "Do it now", 9.5, 106, 100)
-print(book_1.__str__())
+print(book_1)
 
 book_2 = Book("Taras Shevchenko", "Kobzar", 8.2, 405, 450)
-print(book_2.__str__())
+print(book_2)
 
 book_3 = Book("Ivan Franko", "Stonemason", 6.9, 237, 299)
-print(book_3.__str__())
+print(book_3)
 
 print(Book.get_count())
